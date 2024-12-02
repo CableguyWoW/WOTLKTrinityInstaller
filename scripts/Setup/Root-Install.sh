05289f4ef02b97668ad61a7284b12389edc68b1a
CableguyWoW <cableguywow@protonmail.com>
2024-12-02 04:13:09 +0000

    * Fix issues with mysql server install.

--- scripts/Setup/Root-Install.sh
+++ scripts/Setup/Root-Install.sh
@@ -44,10 +44,10 @@ echo "##########################################################"
 echo "## $NUM.Installing Trinity requirements"
 echo "##########################################################"
 echo ""
-apt-get update
+sudo apt update
 wget https://dev.mysql.com/get/mysql-apt-config_0.8.29-1_all.deb -O /tmp/mysql-apt-config_all.deb
 DEBIAN_FRONTEND=noninteractive dpkg -i /tmp/mysql-apt-config_all.deb
-apt-get install git clang cmake make gcc g++ libmysqlclient-dev libssl-dev libbz2-dev libreadline-dev libncurses-dev libboost-all-dev mysql-server p7zip
+sudo apt-get install git clang cmake make gcc g++ libmysqlclient-dev libmysqlclient-dev libssl-dev libbz2-dev libreadline-dev libncurses-dev libboost-all-dev mysql-server p7zip --assume-yes
 update-alternatives --install /usr/bin/cc cc /usr/bin/clang 100
 update-alternatives --install /usr/bin/c++ c++ /usr/bin/clang 100
 fi
