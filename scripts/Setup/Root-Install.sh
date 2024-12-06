43676de29797acbee053ae75fe9ba628d947fca9
CableguyWoW <cableguywow@protonmail.com>
2024-12-06 18:40:57 +0000

    * Fixed wrong sudo username.

--- scripts/Setup/Root-Install.sh
+++ scripts/Setup/Root-Install.sh
@@ -156,7 +156,7 @@ echo ""
 if [ $SETUP_DEV_WORLD == "true" ]; then
 	sudo useradd -m -p $SETUP_REALM_PASS -s /bin/bash $SETUP_REALM_USER
     if ! sudo grep -q "$SETUP_REALM_USER ALL=(ALL) NOPASSWD: ALL" "/etc/sudoers.d/$SETUP_AUTH_USER"; then
-        echo "$SETUP_REALM_USER ALL=(ALL) NOPASSWD: ALL" | sudo tee /etc/sudoers.d/$SETUP_AUTH_USER
+        echo "$SETUP_REALM_USER ALL=(ALL) NOPASSWD: ALL" | sudo tee /etc/sudoers.d/$SETUP_REALM_USER
         echo "Added $SETUP_REALM_USER to sudoers with NOPASSWD."
     fi
     echo "Added $SETUP_REALM_PASS User account"
