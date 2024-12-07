f85a6546f65ba1f59c4625d4f5c11071977869c0
CableguyWoW <cableguywow@protonmail.com>
2024-12-07 02:58:32 +0000

    Update restarter_world_gdb.sh

--- scripts/Restarter/World/GDB/restarter_world_gdb.sh
+++ scripts/Restarter/World/GDB/restarter_world_gdb.sh
@@ -2,5 +2,5 @@ while :
 do
 cd $HOME/server/
 gdb -x=$HOME/server/scripts/Restarter/World/GDB/gdbcommands -batch $HOME/server/bin/worldserver > $HOME/server/logs/Server.log
-mv $HOME/server/logs/Server.log $HOME/logs/crashes/log$(date +\%Y-\%m-\%d-\%H-\%M-\%S).log
+mv $HOME/server/logs/Server.log $HOME/server/logs/crashes/log$(date +\%Y-\%m-\%d-\%H-\%M-\%S).log
 done
